beb7acc2f55396a18fc6a935a165ce99f86e3d44
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2023-09-19 20:11:15 +0300

    Initial commit for whaleback

---
+++ whaleback/src/main/java/fi/lands/whaleback/WhalebackApplication.java
@@ -0,0 +1,13 @@
+package fi.lands.whaleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhalebackApplication.class, args);
+	}
+
+}
